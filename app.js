85f6acc917d5d52802adcf80e8fffbc2c743dd30
Nicholas Zachary <nicholas.zachary@outlook.com>
2019-04-19 03:39:10 -0700

    complete minimal objection set up

--- app.js
+++ app.js
@@ -1,30 +1,35 @@
 const express = require("express");
-var bodyParser = require("body-parser");
 
+// Knex Query Builder and Objection ORM
+const Knex = require("knex");
+const knexDbConfig = require("./util/database");
+const { Model } = require("objection");
+
+// Middleware
+var bodyParser = require("body-parser");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
-
 const cors = require("cors");
-const knex = require("./util/database");
 
+// Router
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 const subscriptionRouter = require("./routes/subscription");
 
 const app = express();
+
+// Initialize knex.
+const knex = Knex(knexConfig.development);
+
+// Bind all Models to the knex instance. You only
+// need to do this once before you use any of
+// your model classes.
+Model.knex(knex);
+
+// Middleware
 app.use(cors());
-
-// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
-// parse application/json
-// app.use(bodyParser.json());
-// app.use(function(req, res) {
-//   res.setHeader("Content-Type", "text/plain");
-//   res.write("you posted:\n");
-//   res.end(JSON.stringify(req.body, null, 2));
-// });
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(
@@ -35,6 +40,7 @@ app.use(
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
+// Routes
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/subscription", subscriptionRouter);
--- controllers/subscription.js
+++ controllers/subscription.js
@@ -1,7 +1,5 @@
 "use strict";
 
-const { Subscription, Invoice } = require("../models/models");
-
 exports.getSubscriptions = (req, res, next) => {
   const allSubscriptions = Subscription.fetchAll();
 
@@ -14,21 +12,5 @@ exports.getSubscriptions = (req, res, next) => {
 };
 
 exports.addSubscription = (req, res, next) => {
-  console.log(req, "hi");
-  // console.log(res, "req");
-  console.log("yo");
   res.send("early blockage add subscription");
-  // new Subscription({
-  //   name: "secondTestName",
-  //   amount: 34056
-  // })
-  //   .save()
-  //   .then(e => {
-  //     console.log("SUCCESS SAVING SUBSCRIPTION", e);
-  //     res.status(400).send("Added subscription - good job.");
-  //   })
-  //   .catch(err => {
-  //     console.log("FAILED INSERTION", err);
-  //     res.status(500).send("failed subscription.");
-  //   });
 };
